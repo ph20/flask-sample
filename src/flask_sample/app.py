7c1138ed4d9c2288abe44866ba7fc83d42e82223
Alexander Grynchuk <agrynchuk@gmail.com>
2020-05-06 16:34:00 +0300

    Added ability gathering static files

--- src/flask_sample/app.py
+++ src/flask_sample/app.py
@@ -3,7 +3,9 @@
 
 from flask import Flask
 from flask import render_template
+from flask.cli import FlaskGroup
 from flask import __version__
+import click
 
 APP_NAME = 'sample flask app'
 
@@ -11,8 +13,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html', flask_ver=__version__)
+
+
+@click.command()
+@click.argument('path')
+@click.group(cls=FlaskGroup)
+def gatherstatick(path):
+    click.echo(path)
 
 
 if __name__ == '__main__':
