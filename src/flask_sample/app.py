609747b9f4f3444dfe7902f42934f46f967e457b
Alexander Grynchuk <agrynchuk@gmail.com>
2020-05-07 22:17:29 +0300

    Added ability export static files to directory

--- src/flask_sample/app.py
+++ src/flask_sample/app.py
@@ -3,25 +3,19 @@
 
 from flask import Flask
 from flask import render_template
-from flask.cli import FlaskGroup
 from flask import __version__
-import click
+import os
 
 APP_NAME = 'sample flask app'
 
-app = Flask(__name__)
+STATIC_DIR = os.path.join(os.path.dirname(__file__), 'static')
+
+app = Flask(__name__, static_folder=STATIC_DIR)
 
 
 @app.route('/')
 def home():
     return render_template('index.html', flask_ver=__version__)
-
-
-@click.command()
-@click.argument('path')
-@click.group(cls=FlaskGroup)
-def gatherstatick(path):
-    click.echo(path)
 
 
 if __name__ == '__main__':
